ec0faef0bd6531b0dc59b9adae9e679370a2a937
Tsabit <tsabitzeta@gmail.com>
2023-06-22 17:21:23 +0700

    flight search option

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import FlightApp from './components/FlightApp';
+
+function App() {
+  return (
+    <React.Fragment>
+      <FlightApp/>
+    </React.Fragment>
+  );
+}
+
+export default App;
